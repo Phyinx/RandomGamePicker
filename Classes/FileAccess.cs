df90e6eee01108b39746638021a64629de98fd87
callum-hendry <callum.hendry@bluefruit.co.uk>
2016-12-13 14:32:05 +0000

    Created blank classes, re-used my FileAccessClass from TRG, created an interface Organised file structure Ready to start writing tests

---
+++ Classes/FileAccess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RandomGamePicker
+{
+    public class FileAccess
+    {
+        /*
+        Untestable using asserts
+        Use Moq to ensure that it is called using normal program operation?
+        See the TRG for examples
+        */
+
+        public string[] ReadCsv(string pathToCsv)
+        { 
+            if (File.Exists(pathToCsv))
+            {
+                string[] csvData = File.ReadAllLines(pathToCsv);
+                return csvData;
+            }
+            else
+            {
+                string[] blank = { "", "NO_CSV" };
+                return blank;
+            }
+        }
+    }
+}
--- RandomGamePicker.Tests/RGP.Tests.cs
+++ RandomGamePicker.Tests/RGP.Tests.cs
@@ -4,11 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace RandomGamePicker.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class RandomGamePickerTests
     {
         [TestMethod]
         public void TestMethod1()
         {
+
         }
     }
 }
