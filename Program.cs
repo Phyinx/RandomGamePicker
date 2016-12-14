2647277c3483e1a83f55095382c4ab0219d07679
callum-hendry <callum.hendry@bluefruit.co.uk>
2016-12-14 14:01:15 +0000

    Added a Games.csv and some Main() code

--- Program.cs
+++ Program.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using RandomGamePicker.Classes;
+using RandomGamePicker.Classes.Interface_Classes;
+using RandomGamePicker.Interfaces;
+
 namespace RandomGamePicker
 {
     class Program
     {
         static void Main(string[] args)
         {
+            CSVAccess FileAccessor = new CSVAccess();
+            GameData Games = new GameData(FileAccessor);
 
+            OutputToConsole ConsoleOutput = new OutputToConsole();
+            Menu MenuSystem = new Menu(ConsoleOutput, Games);
         }
     }
 }
